77cb6aa02960d3b1566d463c93838211ec271ea5
Fantuk <146825482+Fantuk@users.noreply.github.com>
2024-08-15 08:12:58 +0700

    Добавлена кнопка с ссылкой в свайпере (#32)
    * добавил кнопку с ссылкой в свайпер
    
    * добавил логику к карточке все врачи
    
    * исправил логику

--- app/components/DoctorsSwiper/DoctorsSwiper.tsx
+++ app/components/DoctorsSwiper/DoctorsSwiper.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import clsx from "clsx";
+import Link from "next/link";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
@@ -9,11 +9,13 @@ import { doctorsData } from "@/app/data/doctorsData";
 
 import DoctorCard from "../DoctorCard/DoctorCard";
 import SwiperButtons from "../SwiperButtons/SwiperButtons";
+import ArrowButton from "@/app/UI/ArrowButton/ArrowButton";
 
 import "swiper/css";
 import classes from "./doctorsSwiper.module.css";
 
 const DoctorsSwiper = () => {
+  const topDoctors = doctorsData.length > 8 ? doctorsData.slice(0, 8): doctorsData;
   return (
     <section className={classes.slider}>
       <Swiper
@@ -33,17 +35,24 @@ const DoctorsSwiper = () => {
           },
         }}
       >
-        {doctorsData.map((doctor) => (
+        {topDoctors.map((doctor) => (
           <SwiperSlide key={doctor.fullName} className={classes.swiper_slide}>
-              <DoctorCard
-                image={doctor.image}
-                fullName={doctor.fullName}
-                professions={doctor.professions}
-                startWork={doctor.startWork}
-                alt={doctor.alt}
-              />
+            <DoctorCard
+              image={doctor.image}
+              fullName={doctor.fullName}
+              professions={doctor.professions}
+              startWork={doctor.startWork}
+              alt={doctor.alt}
+            />
           </SwiperSlide>
         ))}
+
+        {doctorsData.length > 8 && <SwiperSlide className={classes.swiper_slide}>
+          <Link href="/specialists" className={classes.linkCard}>
+            <ArrowButton text="Все врачи" isFill={true} />
+          </Link>
+        </SwiperSlide>}
+        
       </Swiper>
       <SwiperButtons
         buttons={{
