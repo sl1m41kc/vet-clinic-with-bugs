d3b59d1f6a2e2f87a66970150885eb432eeade04
Fantuk <146825482+Fantuk@users.noreply.github.com>
2024-08-22 17:16:05 +0700

    Исправлена структура not-found (#73)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,27 +1,23 @@
-import clsx from "clsx";
-
 import Link from "next/link";
 
 import LogoSVG from "/public/Svg/logo.svg";
 
-import classes from "./notFound.module.css";
-
 export default function NotFound() {
   return (
-    <section className={clsx("container", classes.content)}>
-      <h1 className={classes.title}>404</h1>
-      <div className={classes.text}>
-        <p className={classes.description}>
+    <section className="container flex flex-col items-center justify-center md:gap-[150px] h-screen text-[--clr1] gap-[60px]">
+      <h1 className="md:text-[12.25em] font-bold text-[--clr3] text-[9em]">404</h1>
+      <div className="flex flex-col md:gap-[30px] text-center gap-[20px]">
+        <p className="text-[1.125em] leading-[130%]">
           Запрашиваемая страница не существует или была удалена.
         </p>
-        <p className={classes.link}>
+        <p className="text-[1.5em]">
           Перейти на{" "}
-          <Link href="/" className={classes.underline}>
+          <Link href="/" className="border-b border-[--clr3]">
             главную страницу
           </Link>
         </p>
       </div>
-      <Link href="/"><LogoSVG className={classes.logo} /></Link>
+      <Link href="/"><LogoSVG className="md:w-[300px] w-[200px]" /></Link>
     </section>
   );
 };
