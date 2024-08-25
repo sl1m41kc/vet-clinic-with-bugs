85990ada62a2e635c61f7632fbf1c7eb4cca7fed
Andrey <andrey2455@gmail.com>
2024-08-25 13:28:33 +0700

    Добавил Suspense для блока Prices на странице с ценами

--- app/(pages)/(clients)/prices/page.tsx
+++ app/(pages)/(clients)/prices/page.tsx
@@ -1,16 +1,19 @@
+import { Suspense } from "react";
 import Hero from "@/app/components/Hero/Hero";
 import Prices from "@/app/components/Prices/Prices";
 import PriceTitle from "@/app/components/PriceTitle/PriceTitle";
 import Vaccines from "@/app/components/Vaccines/Vaccines";
+import Loading from "@/app/UI/Loading/Loading";
 
 export default function Home() {
-    return (
-      <>
-        <Hero/>
-        <PriceTitle/>
-        <Prices/>
-        <Vaccines/>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <Hero />
+      <PriceTitle />
+      <Suspense fallback={<Loading/>}>
+        <Prices />
+      </Suspense>
+      <Vaccines />
+    </>
+  );
+}
---
+++ app/UI/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>Данные загружаются...</div>
+  )
+}
+
+export default Loading
