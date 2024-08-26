e44a950685b5a6dc8fa801fdfd60a262d887a676
Fantuk <146825482+Fantuk@users.noreply.github.com>
2024-08-26 21:46:01 +0700

    Исправлена логика в Title, убран use client в page (#92)

--- app/components/Title/Title.tsx
+++ app/components/Title/Title.tsx
@@ -1,15 +1,18 @@
+"use client";
 import React from "react";
-
+import useResize from "@/app/hooks/useResize";
 import styles from "./title.module.css";
 
 interface IProps {
   marginTop: number;
+  marginTopMobile?: number;
 }
 
-const Title = ({ marginTop }: IProps) => {
+const Title = ({ marginTop, marginTopMobile }: IProps) => {
+  const { width } = useResize();
   return (
     <section className="container">
-      <div className={styles.text} style={{ marginTop: marginTop }}>
+      <div className={styles.text} style={{ marginTop: width > 768 ? marginTop: marginTopMobile }}>
         <p className={styles.moved}>
           Наша цель&nbsp;&mdash; обеспечить{" "}
           <span className={styles.colored}>индивидуальный уход,</span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import Hero from "@/app/components/Hero/Hero";
 import GridAdvantages from "./components/GridAdvantages/GridAdvantages";
 import Advantages from "@/app/components/Advantages/Advantages";
@@ -6,19 +5,17 @@ import Title from "@/app/components/Title/Title";
 import MedicialDirections from "./components/MedicalDirections/MedicalDirections";
 import DoctorsSwiper from "./components/DoctorsSwiper/DoctorsSwiper";
 import Services from "./components/Services/Services";
-import useResize from "./hooks/useResize";
 
 export default function Home() {
-  const { width } = useResize();
   return (
     <>
       <Hero />
       <GridAdvantages />
       <Advantages />
-      <Title marginTop={width > 768 ? 150 : 60}/>
+      <Title marginTop={150} marginTopMobile={60}/>
       <MedicialDirections />
       <DoctorsSwiper/>
-      <Title marginTop={width > 768 ? 150 : 60}/>
+      <Title marginTop={150} marginTopMobile={60}/>
       <Services/>
     </>
   );
