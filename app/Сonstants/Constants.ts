1657ecc1d9271e3fb4858dc533b5de7cc8a5be3a
Andrey <andrey2455@gmail.com>
2024-08-28 21:53:03 +0700

    Предварительная подготовка к релизу

--- app/Сonstants/Constants.ts
+++ app/Сonstants/Constants.ts
@@ -1,1 +1,2 @@
-export const REVALIDATE_TIME: number = (process.env.NODE_ENV === "production") ? 3600 : 10;
+// Срок обновления кэша при fetch-запросах
+export const REVALIDATE_TIME: number = Number(process.env.REVALIDATE_TIME) || 10;
